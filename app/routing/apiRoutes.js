68a7e02c71af6e3ac0b383d53cdc69a8e00e1a9e
Jeff Andony <jeff.andony@gmail.com>
2019-07-09 17:07:57 -0600

    Finished best matches in apiRoutes and started re-designing.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,10 +9,37 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        if (friendsArray.length) {
-            friendsArray.push(req.body);
-            res.json(true);
+        var currUser = req.body;
+        var currUserScores = currUser.scores;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            diff: 1000
+        };
+
+        var totalDiff = 0;
+        
+        for (var i = 0; i < friendsArray.length; i++) {
+            var thisFriend = friendsArray[i];
+            totalDiff = 0;
+
+            for (var j = 0; j < currUserScores.length; j++) {
+                var friendScores = thisFriend.scores;
+                totalDiff += Math.abs(parseInt(friendScores[j]) - parseInt(currUserScores[j]));
+            }
+
+            if (totalDiff < bestMatch.diff) {
+                bestMatch.name = thisFriend.name;
+                bestMatch.photo = thisFriend.photo;
+                bestMatch.diff = totalDiff;
+            }
         }
+
+        friendsArray.push(currUser);
+        res.json({
+            matchName: bestMatch.name,
+            matchPhoto: bestMatch.photo
+        });
     });
 
 };
