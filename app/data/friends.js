2fa84b37e1f856c9006e83b250b9ec31b559b227
Jeff Andony <jeff.andony@gmail.com>
2019-07-09 17:42:26 -0600

    updated friends array and added a bootswatch theme.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,48 @@ var friendsArray = [
         name: "Jeff",
         photo: "https://media.licdn.com/dms/image/C4E03AQE8HnSYolT1zg/profile-displayphoto-shrink_200_200/0?e=1568246400&v=beta&t=ap77_RdwGEr3ZJcB28N2Ai2cIjo7iaql0E3bMLLgObA",
         scores: [
+            "1",
             "5",
             "1",
+            "3",
+            "1",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Mr. Meme",
+        photo: "https://i.chzbgr.com/full/1776896768/h104964F6/",
+        scores: [
+            "5",
+            "3",
+            "3",
             "4",
-            "4",
+            "2",
+            "1",
+            "2",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Pup Jr.",
+        photo: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        scores: [
+            "1",
+            "2",
+            "3",
             "5",
             "1",
             "2",
-            "5",
+            "3",
             "4",
-            "1"
+            "1",
+            "2"
         ]
     }
 ];
